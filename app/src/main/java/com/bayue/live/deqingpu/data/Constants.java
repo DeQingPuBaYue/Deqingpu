20289fbe9730906a50a85d01f5eedbe17ca0e9e5
haoyuexiyu <907070466@qq.com>
2017-06-07 12:53:03 +0800

    add api

--- app/src/main/java/com/bayue/live/deqingpu/data/Constants.java
+++ app/src/main/java/com/bayue/live/deqingpu/data/Constants.java
@@ -1,5 +1,8 @@
 package com.bayue.live.deqingpu.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Kevin on 2016/11/28.
  * Blog:http://blog.csdn.net/student9128
@@ -8,4 +11,10 @@ package com.bayue.live.deqingpu.data;
 
 public class Constants {
     public static final String ARGS = "args";
+    private static Map<String, Object> map = new HashMap<>();
+    public static Map<String, Object> getMap(){
+        map.put("safecode","BaYue.deqingpu");
+        map.put("apiversion","v.1.0");
+        return map;
+    }
 }
